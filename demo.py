1ae12b4adcd6a2e2e6dc906bbe295d9e0818d22c
KhannaSahab16 <139047365+KhannaSahab16@users.noreply.github.com>
2025-06-06 17:41:45 +0530

    save

--- demo.py
+++ demo.py
@@ -1,4 +1,10 @@
-print("Hello World")
-print("Hello World")
-print("yo yo yo")
-print("This is a demo script.")
+
+import pyjokes
+
+print("Printing jokes....")
+#print(Printing jokes....)
+joke = pyjokes.get_joke()
+print(joke)
+
+""" so thanks that's my demo for pyjokes
+    I hope you enjoyed it!"""
