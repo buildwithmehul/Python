98fcdd8ff8493d8691a5ee04ce62d4e282acaa79
KhannaSahab16 <139047365+KhannaSahab16@users.noreply.github.com>
2025-06-06 18:07:46 +0530

    this a program using pip pyttsx3

---
+++ prob1.py
@@ -0,0 +1,5 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("This program is using pyttsx3 to convert text to speech.")
+engine.say("It is a simple example of text-to-speech conversion.")
+engine.runAndWait()
