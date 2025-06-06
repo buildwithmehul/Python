85f9ab3be6667b5773cbfc545b8594ae11b2d9fc
KhannaSahab16 <139047365+KhannaSahab16@users.noreply.github.com>
2025-06-07 01:32:56 +0530

    Porogram on Operators and Inputs

---
+++ operatorsandinputs.py
@@ -0,0 +1,62 @@
+# program to demonstrate the use of different operators in Python
+
+
+# Arithmetic Operators
+a = 7
+b = 4
+c = a + b
+print(c)
+# Assignment Operators
+a = 4-2 # Assign 4-2 in a
+print(a)
+b = 6
+# b += 3 # Increment the value of b by 3 and then assign it to b
+b -= 3 # Decrement the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+d = 5 > 3
+print(d)
+
+d = 5 < 3
+print(d)
+
+d = 5==5
+print(d)
+
+d = 5 != 3
+print(d)
+
+
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and'
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
+
+
+# Program to demonstrate the use of input function and arithmetic operations
+""" 
+Basically, when we take input using input function, it takes input as a string. So we need to convert it to an integer using int() function.
+If we don't convert it, it is string so concatenation will happen instead of addition.
+"""
+a = int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Sum is ", a + b)
